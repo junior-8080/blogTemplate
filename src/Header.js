86d92b3cd2e7dc7625caa5e1adab581afcea7c36
Liman Adamu Labaran <liman4u@live.com>
2020-04-26 13:06:00 +0200

    fix issues with redundant img alt and api-key usage

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Search from "./Search"
-import Features from "./Features"
 import "./header.css"
 
 function Header(props){
--- src/Search.js
+++ src/Search.js
@@ -4,10 +4,11 @@ import "./search.css"
 
 function Search(props) {
     return (
-    <form onSubmit={props.handleSubmit}>
-        <input type = "text" placeholder = "Enter Query" onChange = {props.handleChange} />
-        <i class="fas fa-search"></i>
-    </form>
+	    <form onSubmit={props.handleSubmit}>
+	        <input type = "text" placeholder = "Enter Query" onChange = {props.handleChange} />
+	        <i className="fas fa-search"></i>
+	    </form>
     )
 }
+
 export default Search
